74ec650cd4e796dfecfcb962ea25bbc39804dc7f
your name <you@example.com>
2024-04-26 21:55:34 -0400

    add apache commons; format log messages

--- th-ui/src/main/java/com/dg/apps/th/ui/tools/JTextAreaAppender.java
+++ th-ui/src/main/java/com/dg/apps/th/ui/tools/JTextAreaAppender.java
@@ -2,12 +2,19 @@ package com.dg.apps.th.ui.tools;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 public class JTextAreaAppender extends AppenderBase<ILoggingEvent> {
+    private static final String REALTIME = "{REALTIME}";
+    private static final String QUEUED   = "{QUEUED}  ";
+
     private JTextArea _txtArea;
 
     private final List<ILoggingEvent> _lstQueue = new ArrayList<>();
@@ -34,14 +41,32 @@ public class JTextAreaAppender extends AppenderBase<ILoggingEvent> {
         }
 
         if (!_lstQueue.isEmpty()) {
-            for (ILoggingEvent queuedEvent : _lstQueue) {
-                final String queuedMessage = queuedEvent.getFormattedMessage();
-                _txtArea.append("{QUEUED}    " + queuedMessage + "\n");
-            }
+            _lstQueue.forEach(queuedEvent -> _txtArea.append(format(queuedEvent, QUEUED)));
             _lstQueue.clear();
         }
 
-        final String message = event.getFormattedMessage();
-        _txtArea.append("{REALTIME}  " + message + "\n");
+        _txtArea.append(format(event, REALTIME));
+    }
+
+    private String format(final ILoggingEvent event, final String when) {
+        final LocalDateTime ldt = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.getTimeStamp()), TimeZone.getDefault().toZoneId());
+
+        final String time = ldt.toLocalTime().toString();
+        final String level = StringUtils.rightPad(event.getLevel().levelStr, 5, ' ');
+        final String thread = StringUtils.rightPad(event.getThreadName(), 10, ' ');
+
+        return new StringBuilder()
+                .append(when)
+                .append(" ")
+                .append("[")
+                .append(time)
+                .append("][")
+                .append(level)
+                .append("][")
+                .append(thread)
+                .append("]:  ")
+                .append(event.getMessage())
+                .append("\n")
+                .toString();
     }
 }
