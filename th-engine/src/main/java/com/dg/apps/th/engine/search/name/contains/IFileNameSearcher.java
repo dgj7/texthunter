4a88404386c922192f09591c6f4c7d220666f377
your name <you@example.com>
2024-04-30 22:19:03 -0400

    move factories

--- th-engine/src/main/java/com/dg/apps/th/engine/search/name/contains/IFileNameSearcher.java
+++ th-engine/src/main/java/com/dg/apps/th/engine/search/name/contains/IFileNameSearcher.java
@@ -1,6 +1,11 @@
 package com.dg.apps.th.engine.search.name.contains;
 
+import com.dg.apps.th.engine.search.name.contains.impl.CaseInsensitiveFileNameSearcher;
+import com.dg.apps.th.engine.search.name.contains.impl.CaseSensitiveFileNameSearcher;
+import com.dg.apps.th.engine.search.name.contains.impl.DisabledFileNameSearcher;
+import com.dg.apps.th.engine.search.name.contains.impl.RegexFileNameSearcher;
 import com.dg.apps.th.model.config.SearchConfiguration;
+import com.dg.apps.th.model.def.FileNameFilterResult;
 import com.dg.apps.th.model.def.FileNameSearchResult;
 
 import java.io.File;
@@ -13,4 +18,24 @@ public interface IFileNameSearcher {
      * Search a file's name.
      */
     FileNameSearchResult searchFileName(final File file, final SearchConfiguration config);
+
+    /**
+     * Provide {@link IFileNameSearcher}.
+     */
+    static IFileNameSearcher getFileNameSearcher(final SearchConfiguration config, final FileNameFilterResult fileNameFilterResult) {
+        if (FileNameFilterResult.Failed.equals(fileNameFilterResult))
+            return DisabledFileNameSearcher.getInstance();
+
+        if (config.isSearchFileNames()) {
+            if (config.isRegexSearchString()) {
+                return RegexFileNameSearcher.getInstance();
+            } else {
+                if (config.isCaseSensitive())
+                    return CaseSensitiveFileNameSearcher.getInstance();
+                else
+                    return CaseInsensitiveFileNameSearcher.getInstance();
+            }
+        }
+        return DisabledFileNameSearcher.getInstance();
+    }
 }
--- th-engine/src/main/java/com/dg/apps/th/engine/search/name/filter/IFileNameFilterer.java
+++ th-engine/src/main/java/com/dg/apps/th/engine/search/name/filter/IFileNameFilterer.java
@@ -1,5 +1,9 @@
 package com.dg.apps.th.engine.search.name.filter;
 
+import com.dg.apps.th.engine.search.name.filter.impl.CaseInsensitiveFileNameFilterer;
+import com.dg.apps.th.engine.search.name.filter.impl.CaseSensitiveFileNameFilterer;
+import com.dg.apps.th.engine.search.name.filter.impl.DisabledFileNameFilterer;
+import com.dg.apps.th.engine.search.name.filter.impl.RegexFileNameFilterer;
 import com.dg.apps.th.model.config.SearchConfiguration;
 import com.dg.apps.th.model.def.FileNameFilterResult;
 
@@ -13,4 +17,21 @@ public interface IFileNameFilterer {
      * Filter a filename.
      */
     FileNameFilterResult filterFileName(final File file, final SearchConfiguration config);
+
+    /**
+     * Provide {@link IFileNameFilterer}.
+     */
+    static IFileNameFilterer getFileNameFilterer(final SearchConfiguration config) {
+        if (config.isFilteredSearch()) {
+            if (config.isRegexFileNameFilter()) {
+                return RegexFileNameFilterer.getInstance();
+            } else {
+                if (config.isCaseSensitive())
+                    return CaseSensitiveFileNameFilterer.getInstance();
+                else
+                    return CaseInsensitiveFileNameFilterer.getInstance();
+            }
+        }
+        return DisabledFileNameFilterer.getInstance();
+    }
 }
