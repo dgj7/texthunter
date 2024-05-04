f591c593afdd9803008e6e6291541fb2e3d139d6
dgj7 <garciad48879@gmail.com>
2024-05-03 20:55:09 -0400

    documentation; cleanup

--- th-engine/src/main/java/com/dg/apps/th/engine/search/content/ISearch.java
+++ th-engine/src/main/java/com/dg/apps/th/engine/search/content/ISearch.java
@@ -8,7 +8,23 @@ import com.dg.apps.th.model.adapter.ITableAdapter;
 import com.dg.apps.th.model.config.SearchConfiguration;
 
 /**
+ * <p>
  * Main handle to search.
+ * </p>
+ * <p>
+ * Extends {@link Runnable}, as it's main method is {@code run()},
+ * and we want to launch the search process in it's own thread.  This
+ * keeps the search ui thread responsive.
+ * </p>
+ * <p>
+ * Adds a method that can be called from a button, to halt the process
+ * and return.
+ * </p>
+ * <p>
+ * It's expected that this process won't return any value.  Instead, it'll
+ * utilise various injected interfaces to contact the ui for updates
+ * in real time.
+ * </p>
  */
 public interface ISearch extends Runnable {
     /**
