dca810a0aa83411fbed0b68f9ad7e5118ad03b17
dgj7 <garciad48879@gmail.com>
2025-09-07 00:56:30 -0400

    java: trigger build

--- java/start.sh
+++ java/start.sh
@@ -2,7 +2,14 @@
 
 # build the app (maybe)
 if [ ! -f th-ui/target/th-ui-0.0.1-jar-with-dependencies.jar ]; then
-    ./silent.sh
+    printf "building ..."
+    (./rebuild.sh > /dev/null 2>&1)
+    if [ $? -eq 0 ]; then
+        printf " done.\n"
+    else
+        printf " failed!\n"
+        exit 1
+    fi
 else
     printf "no build needed, skipping.\n"
 fi
